06494ca309389b3da2903988e927843584718ccc
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-06-04 10:54:51 +0300

    Add dotenv dependency and configure environment variables in server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
 import type { Request, Response } from 'express';
 import express from 'express';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
