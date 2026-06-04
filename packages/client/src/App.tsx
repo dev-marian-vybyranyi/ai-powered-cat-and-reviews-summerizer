3e0850bf7ae9be818285ea21716c422daa8892c0
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-06-04 13:42:25 +0300

    add ChatBot component and integrate react-icons; update package dependencies

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from 'react';
-import { Button } from './components/ui/button';
+import ChatBot from './components/ChatBot';
 
 function App() {
-   const [message, setMessage] = useState(0);
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   }, []);
-
    return (
-      <div className="App">
-         <h1 className="font-bold p-4 text-3xl">{message}</h1>
-         <Button variant="outline">Button</Button>
+      <div className="p-4">
+         <ChatBot />
       </div>
    );
 }
